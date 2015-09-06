6c28720bfab76bdfa882eb7d03e6694efa27aaee
purple <purpletianjing@163.com>
2015-09-06 11:35:01 +0800

    delete annotation

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,22 +1,3 @@
-/*function sameEle(ele,object_b)  {
-  for (var i = 0; i < object_b.value.length; i++) {
-    if(ele === object_b.value[i])  {
-      return ele;
-    }
-  }
-}
-function collect_same_elements(collection_a, object_b) {
-  var temp;
-  var result = [];
-  for (var i = 0; i < collection_a.length; i++) {
-    temp = sameEle(collection_a[i],object_b);
-    if(temp !== undefined)  {
-      result.push(temp);
-    }
-  }
-  return result;
-}
-var result = collect_same_elements(collection_a,collection_b);*/
 function collect_same_elements(collection_a,object_b) {
   return collection_a.filter(function(item){
     return object_b.value.indexOf(item) != -1;
