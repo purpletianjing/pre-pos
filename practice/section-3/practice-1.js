9d4ae32c08ce0f1472ab94d5847282fad69ea003
purple <purpletianjing@163.com>
2015-09-06 11:35:53 +0800

    delete annotation

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,12 +1,3 @@
-// function judgeEle(ele,object_b) {
-//   for (var i = 0; i < object_b.value.length; i++) {
-//     if(ele.key === object_b.value[i]) {
-//       ele.count--;
-//       return;
-//     }
-//   }
-//   return;
-// }
 function create_updated_collection(collection_a, object_b) {
   for (var i = 0; i < collection_a.length; i++) {
     if(object_b.value.indexOf(collection_a[i].key) !== -1) {
