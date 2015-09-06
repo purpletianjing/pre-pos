bf210b7d83d9724d5c65460aab2856af6ad9c2e6
purple <purpletianjing@163.com>
2015-09-06 11:35:33 +0800

    delete annotation

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,38 +1,3 @@
-// function findEle(ele,result) {
-//   var temp = 1;
-//   var sum = 0;
-//     for (var i = 0; i < result.length; i++) {
-//     if(ele[0] === result[i].name) {
-//       if(ele.length > 1) {
-//         if(ele.length > 3) {
-//           for(var k = ele.length - 2;k >= 2;k--) {
-//             sum =sum + temp * parseInt(ele[k]);
-//             temp = temp * 10;
-//           }
-//           result[i].summary += sum;
-//           return;
-//         }
-//         result[i].summary += parseInt(ele[2]);
-//         return;
-//       }
-//       result[i].summary++;
-//       return;
-//     }
-//   }
-//   if(ele.length > 1) {
-//     result.push({name:ele[0],summary:parseInt(ele[2])});
-//     return;
-//   }
-//   result.push({name:ele,summary:1});
-// }
-// function count_same_elements(collection) {
-//   var result = [];
-//   for (var i = 0; i < collection.length;i++) {
-//     findEle(collection[i],result);
-//   }
-//   return result;
-// }
-// count_same_elements(collection);
 function count_same_elements(collection) {
   var result = [];
   var counts = {};
@@ -50,9 +15,6 @@ function formatItem(item) {
   if (item.length === 1) {
     return { char: item, count: 1 };
   }
-  // } else if (item[1] === '[') {
-  //   return { char: item[0], count: parseInt(item.substring(2, item.length - 1)) };
-  // }
   else {
     return { char: item[0], count: parseInt(item.slice(2)) };
   }
