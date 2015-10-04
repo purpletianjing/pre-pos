e45bb9392133836ba958dfac0d4421b99d3553f9
purple <purpletianjing@163.com>
2015-10-04 12:15:09 +0800

    modify the first for to forEach

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,10 +1,10 @@
 function count_same_elements(collection) {
   var result = [];
   var objResult = {};
-  for (var i = 0; i < collection.length;i++) {
-    objResult[collection[i]] = (objResult[collection[i]] + 1) || 1;
-  }
-  for (var j in objResult) {
+  collection.forEach(function (val) {
+    objResult[val] = objResult[val] + 1 || 1;
+  });
+   for (var j in objResult) {
     result.push({key:j,count:objResult[j]});
   }
   return result;
