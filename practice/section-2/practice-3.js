cfe7f55968212da0e331e1b43af3437b6f6e5892
purple <purpletianjing@163.com>
2015-10-04 12:40:24 +0800

    modify the first for to forEach

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,10 +1,12 @@
 function count_same_elements(collection) {
   var result = [];
   var counts = {};
-  for (var i = 0; i < collection.length; i++) {
-    var item = formatItem(collection[i]);
-    counts[item.char] = (counts[item.char] + item.count) || item.count;
-  }
+  var item;
+
+  collection.forEach(function (value) {
+    item = formatItem(value);
+    counts[item.char] = counts[item.char] + item.count || item.count;
+  });
   for (var char in counts) {
     result.push({ name: char, summary: counts[char] });
   }
